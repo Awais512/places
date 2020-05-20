1533d9e2f98eb3fd1da83dfb21266665f8b2af27
awais512 <raza.awais25@gmail.com>
2020-05-20 16:27:38 +0500

    Adding Modal

--- src/App.js
+++ src/App.js
@@ -8,20 +8,26 @@ import {
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 function App() {
   return (
     <Router>
       <MainNavigation />
-      <Switch>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Route path='/places/new' exact>
-          <NewPlace />
-        </Route>
-        <Redirect to='/' />
-      </Switch>
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Users />
+          </Route>
+          <Route path='/:userId/places' exact>
+            <UserPlaces />
+          </Route>
+          <Route path='/places/new' exact>
+            <NewPlace />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </Router>
   );
 }
